016ccab32894ccdee3ab8121f33e577869b7e22a
lisgie <livio@localhost.localdomain>
2015-04-11 16:33:28 +0200

    Extending file structure by blocks and file classes

---
+++ src/main/challengetask/group02/fsstructure/Block.java
@@ -0,0 +1,26 @@
+package challengetask.group02.fsstructure;
+
+import net.tomp2p.peers.Number160;
+
+public class Block {
+	
+	public static final int BLOCK_SIZE = 1024;
+	
+	private Number160 ID;
+	//used to identify the position of a block within a file
+	private long seq_number; 
+	//used for error detection
+	private int checksum; 
+	private byte[] data;
+	
+	public Block(Number160 ID, long seq_number, int checksum, byte[] data) {
+		
+		//the data is probably going to be assigned via copy-by-reference, no need to allocate memory
+		
+		this.ID = ID;
+		this.seq_number = seq_number;
+		this.checksum = checksum;
+		this.data = data;		
+	}		
+
+}
---
+++ src/main/challengetask/group02/fsstructure/File.java
@@ -0,0 +1,13 @@
+package challengetask.group02.fsstructure;
+
+import challengetask.group02.fsstructure.Entry;
+import java.util.ArrayList;
+
+public class File extends Entry {
+	
+	private boolean complete_flag;
+	private ArrayList<Block> blocks;	
+	
+	//Like discussed, calculating and fetching data is done via controller classes
+	
+}

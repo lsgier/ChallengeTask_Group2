eef03e30494c186afec8697edbc0ee9fb698ac83
Anvar T <anvar.tukhvatulin@gmail.com>
2015-05-22 11:08:33 +0200

    odroid runner script

---
+++ runner/standalone-odroid.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 mountpoint [bootstrapIP]"
+	exit 1
+fi
+
+mountPoint="$1"
+
+if [ -z "$1" ]
+  then
+    echo "Mount point is not specified"
+    exit 1
+fi
+
+if [ "$(ls -A $1)" ]; then
+   echo "Mount point is not empty"
+   exit 1
+fi
+
+if [ -z "$2" ]
+  then
+     java -cp p2pfs.jar -Xmx512M -XX:MaxDirectMemorySize=512M prototype.FSPeer "$mountPoint" >/dev/null 2>&1 &
+  else
+    java -cp p2pfs.jar -Xmx512M -XX:MaxDirectMemorySize=512M prototype.FSPeer "$mountPoint" "$2" >/dev/null 2>&1 &
+fi
